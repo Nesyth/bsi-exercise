3d06b5adbc25bf7d4d47d90065140e326ccac758
nesyth <n3syth@gmail.com>
2021-11-16 15:07:10 +0100

    Added menu

--- main.cpp
+++ main.cpp
@@ -13,11 +13,48 @@ int main() {
 //        v.push_back(input);
 //    cout << average(v) << endl << standardDeviation(v);
 
-//    int n, k;
-//    cin >> n >> k;
-//    cout << binomialCoefficient(n, k);
+    char selection;
 
-    constant_failure_rate();
+    do {
+        cout << "=================================\n";
+        cout << "1.  Manual Switches\n";
+        cout << "2.  Probability Insulators\n";
+        cout << "3.  xxx\n";
+        cout << "4.  xxx\n";
+        cout << "5.  Constant Failure Rate\n";
+        cout << "\n";
+        cout << "9.  Exit\n";
+        cout << "=================================\n";
+        cout << "Enter your selection: ";
+        cin >> selection;
+        cout << endl;
+
+        switch (selection) {
+            case '1':
+                cout << "todo";
+                break;
+            case '2':
+                probability_insulators();
+                break;
+            case '3':
+                cout << "todo";
+                break;
+            case '4':
+                cout << "todo";
+                break;
+            case '5':
+                constant_failure_rate();
+                break;
+            case '9':
+                break;
+            default:
+                cout << selection << " is not a valid menu item.\n\n";
+        }
+    }
+
+    while (selection != '9');
+
     return 0;
 }
+
 
